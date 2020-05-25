4360befc9c4c2e366dc42d3f867f01934c38429a
baimin <xiaofengbm@gmail.com>
2020-05-25 16:21:25 +0800

    feat(权限测试页): 添加权限测试页

---
+++ src/views/permission/RMD.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>超级管理员（admin）和全国总监（RMD）能看到</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+
+@Component({
+  name: ""
+})
+export default class extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/permission/page.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container">
+    <div>
+      <h3>当前登录者为{{ currentName }}</h3>
+      <span>拥有权限为{{ currentRoles }}， admin具有全部权限</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+import { UserModule } from '@/store/modules/user';
+
+@Component({
+  name: "name"
+})
+export default class extends Vue {
+  private currentName = UserModule.name;
+  private currentRoles = UserModule.roles
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
